771d1f627062769b580b3d442599d41b1aba0141
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2023-02-02 21:31:20 +0100

    Wrap in loop, stupid

--- backend/import_calendars.py
+++ backend/import_calendars.py
@@ -10,25 +10,28 @@ from time import sleep
 year = 2023
 
 with app.app_context():
-    try:
-        addrs = Address.query.filter(Address.mitaffald_id.isnot(None)).all()
-        shuffle(addrs)
-        for addr in addrs:
-            mitaffald_id = addr.mitaffald_id
-            # TODO Use 'resolve_calendar' instead to avoid duplication
-            #      and also because it is going to insert NULL on failure.
-            res = Calendar.query.filter_by(mitaffald_id=mitaffald_id, year=year).first()
-            if res:
-                print('skipping', mitaffald_id)
-                continue
-            print('fetching', mitaffald_id)
-            html = fetch_calendar_html(str(mitaffald_id), year)
-            calendar, parser_version = parse_calendar(html)
-            if calendar:
-                db.session.add(
-                    Calendar(mitaffald_id=mitaffald_id, year=year, json=calendar, parser_version=parser_version))
-                db.session.commit()
-    except Exception as e:
-        print('encountered error:', e)
-        print('restarting in 60s')
-        sleep(60)
+    while True:
+        try:
+            addrs = Address.query.filter(Address.mitaffald_id.isnot(None)).all()
+            shuffle(addrs)
+            for addr in addrs:
+                mitaffald_id = addr.mitaffald_id
+                # TODO Use 'resolve_calendar' instead to avoid duplication
+                #      and also because it is going to insert NULL on failure.
+                res = Calendar.query.filter_by(mitaffald_id=mitaffald_id, year=year).first()
+                if res:
+                    print('skipping', mitaffald_id)
+                    continue
+                print('fetching', mitaffald_id)
+                html = fetch_calendar_html(str(mitaffald_id), year)
+                calendar, parser_version = parse_calendar(html)
+                if calendar:
+                    db.session.add(
+                        Calendar(mitaffald_id=mitaffald_id, year=year, json=calendar, parser_version=parser_version))
+                    db.session.commit()
+            print('all done!')
+            break
+        except Exception as e:
+            print('encountered error:', e)
+            print('restarting in 60s')
+            sleep(60)
