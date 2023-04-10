b3987e08df7b3628972755f601765cb692974789
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2023-04-11 00:17:04 +0200

    Dump the values of all config fields on the page
    For debugging... Could polish to put this into a component that's only available in dev mode...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,14 @@ import { useRef } from 'react';
 import { useDawaAutocomplete } from './useDawaAutocomplete';
 import { useMitaffaldAddressId } from './useMitaffaldAddressId';
 import { AddressCalendar } from './AddressCalendar';
-import { YEAR_FIRST_WEEKDAY_IDX, YEAR_IS_LEAP_YEAR, YEAR, MITAFFALD_URL_VIEW_ADDRESS } from './config';
+import {
+    YEAR_FIRST_WEEKDAY_IDX,
+    YEAR_IS_LEAP_YEAR,
+    YEAR,
+    MITAFFALD_URL_VIEW_ADDRESS,
+    BACKEND_URL_BASE,
+    DAWA_URL_QUERY_ADDRESS, DAWA_KOMMUNEKODE
+} from './config';
 
 function showAddressInfoUrl(mitaffaldId) {
     const url = new URL(MITAFFALD_URL_VIEW_ADDRESS);
@@ -19,6 +26,15 @@ export default function App() {
             <div className="rounded-xl border p-3 mt-4 mb-6 bg-stone-200">
                 <h1 className="text-xl">Affaldskalender for Aarhus Kommune</h1>
             </div>
+            <h3>Config</h3>
+            <ul>
+                <li>BACKEND_URL_BASE: {BACKEND_URL_BASE}</li>
+                <li>DAWA_URL_QUERY_ADDRESS: {DAWA_URL_QUERY_ADDRESS}</li>
+                <li>DAWA_KOMMUNEKODE: {DAWA_KOMMUNEKODE}</li>
+                <li>YEAR: {YEAR}</li>
+                <li>YEAR_IS_LEAP_YEAR: {YEAR_IS_LEAP_YEAR}</li>
+                <li>YEAR_FIRST_WEEKDAY_IDX: {YEAR_FIRST_WEEKDAY_IDX}</li>
+            </ul>
             <p className="relative w-full">
                 <label htmlFor="input" className="text-lg">
                     Indtast adresse:
